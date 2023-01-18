96020839f40cb6a8ac45e1114f1b4eeb717261e2
lelion solonka <122529284+LelionS@users.noreply.github.com>
2023-01-18 08:52:52 +0000

    Odd and Even numbers using javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+//even numbers between 0 to 20
+console.log("even numbers between 0 to 20;")
+for (i=0;i<=20;i++){
+    if  (
+      i % 2 == 0
+    ) 
+      console.log(i)
+}
+
+//odd
+console.log("odd numbers between 0 to 20;")
+for (j=0; j<=20; j++){
+  if(
+    j % 2 ==!0)
+    console.log(j)
+}
+
